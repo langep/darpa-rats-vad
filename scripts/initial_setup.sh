c12fefbb6741148a6535edb5eca7effd971d2852
langep <patrick.l.lange@gmail.com>
2017-12-06 22:57:08 -0800

    Added configuring of data location

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -4,10 +4,10 @@ IFS=$'\n\t'
 
 # Taken from https://dev.to/thiht/shell-scripts-matter 
 
-#/ Usage: bash inital_setup.sh KALDI_ROOT
+#/ Usage: bash inital_setup.sh KALDI_ROOT RATS_DATA
 #/ Description: Runs the initial experiment setup given a path 
-#/   to a working kaldi installation.
-#/ Examples: bash initial_setup.sh /opt/tools/kaldi
+#/   to a working kaldi installation and the DARPA RATS data.
+#/ Examples: bash initial_setup.sh /opt/tools/kaldi /opt/data/DARPA_RATS/data
 #/ Options:
 #/   --help: Display this help message
 usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
@@ -50,15 +50,18 @@ copy_from_existing_experiment() {
 			fi
 		fi
 	done
+
+	echo $RATS_DATA > .data_dir
 }
 
 
 # Prevent execution when sourcing this script
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
-	if [[ $# -ne 1 ]]; then
+	if [[ $# -ne 2 ]]; then
 		usage
 	else
 		KALDI_ROOT=$1
+		RATS_DATA=$2
 	fi
 
 	locate_self
