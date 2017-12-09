c038fcd6fd9333044538d632054b4ec08ce0abbc
langep <patrick.l.lange@gmail.com>
2017-12-08 21:28:16 -0800

    Adding merging of files

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -23,27 +23,50 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 
 	datadir=$(cat .data_dir)
 
-	# Split the training audio
+	stage=2
+
+
 	train_audio=$datadir/train/audio/eng
 	train_tab=${datadir}/train/sad/eng
 	local_data=data
 	local_train=data/train
-	for channel in A B C D E F G H src; do
-		( # Run each channel in a background task to speed up the work.
+	channels="A B C D E F G H src"
+	classes="NS NT S RX"
+	used_classes="NS NT S"
 
-		# Make the directories for each class
-		for class in NS NT S RX; do 
-			mkdir -p $local_train/$channel/$class
-		done
-		for audiofile in $train_audio/$channel/*.flac; do
-			name=$(basename $audiofile)
-			name_wo_ext=${name%.*} # non-greedy removal from end
-			tabfile=$train_tab/$channel/$name_wo_ext.tab
-			bash $SCRIPT_DIR/clip_helper.sh $audiofile $tabfile $local_train/$channel
-			echo "$audiofile done" # Progress indicator
-		done
-		) &
-	done
+	if [ $stage -le 1 ]; then
+		# Split the training audio
 
+		for channel in "$channels"; do
+			( # Run each channel in a background task to speed up the work.
+
+			# Make the directories for each class
+			for class in "$classes"; do 
+				mkdir -p $local_train/$channel/$class
+			done
+			for audiofile in $train_audio/$channel/*.flac; do
+				name=$(basename $audiofile)
+				name_wo_ext=${name%.*} # non-greedy removal from end
+				tabfile=$train_tab/$channel/$name_wo_ext.tab
+				bash $SCRIPT_DIR/clip_helper.sh $audiofile $tabfile $local_train/$channel
+				echo "$audiofile done" # Progress indicator
+			done
+			) &
+		done
+	fi
+
+	# Join all the snippets again but now one we get one audio file per channel and used class
+	if [ $stage -le 2 ]; then
+		for channel in "$channels"; do
+			for class in "$used_classes"; do
+				# Start out with small amount of silence
+				sox -n -b 16 -r 16000 -c 1 $local_train/$channel/$class.wav trim 0 0.1
+				for file in $local_train/$channel/$class/*.wav; do
+					sox $local_train/$channel/$class.wav $file $local_train/$channel/_$class.wav
+					mv $local_train/$channel/_$class.wav $local_train/$channel/$class.wav
+				done
+			done
+		done
+	fi
 fi
 
