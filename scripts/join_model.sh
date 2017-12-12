149d9ea2aeaabb3c2d8914cc4b34d5aec8b886aa
langep <patrick.l.lange@gmail.com>
2017-12-11 23:26:10 -0800

    make join_model in two steps to manually fix utt2spk

--- scripts/join_model.sh
+++ scripts/join_model.sh
@@ -29,36 +29,42 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 	channels="A B C D E F G H src"
 	used_classes="NS NT S"
 
-	for class in $used_classes; do
-		mkdir -p $class/all
-		for channel in $channels; do
-			if [ $class == "NT" ]; then
-				if [ $channel == "G" ] || [ $channel == "src" ]; then
-					continue
+	stage=1
+	if [ $stage -eq 1 ]
+		for class in $used_classes; do
+			mkdir -p $class/all
+			for channel in $channels; do
+				if [ $class == "NT" ]; then
+					if [ $channel == "G" ] || [ $channel == "src" ]; then
+						continue
+					fi
 				fi
-			fi
-			sed -e "s/^/${channel}_/g" $class/$channel/wav.scp >> $class/all/wav.scp1
-			sed -e "s/^/${channel}_/g" $class/$channel/feats.scp >> $class/all/feats.scp1
-			sed -e "s/^/${channel}_/g" $class/$channel/vad.scp >> $class/all/vad.scp1
-			sed -e "s/$class/${channel}_${class}/g" $class/$channel/utt2spk >> $class/all/utt2spk1
-				
+				sed -e "s/^/${channel}_/g" $class/$channel/wav.scp >> $class/all/wav.scp1
+				sed -e "s/^/${channel}_/g" $class/$channel/feats.scp >> $class/all/feats.scp1
+				sed -e "s/^/${channel}_/g" $class/$channel/vad.scp >> $class/all/vad.scp1
+				sed -e "s/$class/${channel}_${class}/g" $class/$channel/utt2spk >> $class/all/utt2spk1
+					
+			done
+
+			cat $class/all/wav.scp1 | sort > $class/all/wav.scp
+			cat $class/all/feats.scp1 | sort > $class/all/feats.scp
+			cat $class/all/vad.scp1 | sort > $class/all/vad.scp
+			cat $class/all/utt2spk1 | sort > $class/all/utt2spk
+
 		done
+	fi
 
-		cat $class/all/wav.scp1 | sort > $class/all/wav.scp
-		cat $class/all/feats.scp1 | sort > $class/all/feats.scp
-		cat $class/all/vad.scp1 | sort > $class/all/vad.scp
-		cat $class/all/utt2spk1 | sort > $class/all/utt2spk
-
-		./utils/utt2spk_to_spk2utt.pl $class/all/utt2spk > $class/all/spk2utt
-		bash utils/validate_data_dir.sh --no-text $class/all
-
-		sid/train_diag_ubm.sh --nj $nj --cmd "$train_cmd" --delta-window 2 \
-			$class/all 32 exp/diag_ubm_${class}_all
-		
-		sid/train_full_ubm.sh --nj $nj --cmd "$train_cmd" \
-			--remove-low-count-gaussians false $class/all \
-			exp/diag_ubm_${class}_all exp/full_ubm_${class}_all
-	done
+	if [ $stage -eq 2 ]; then
+		for class in $used_classes; do
+			./utils/utt2spk_to_spk2utt.pl $class/all/utt2spk > $class/all/spk2utt
+			bash utils/validate_data_dir.sh --no-text $class/all
+			sid/train_diag_ubm.sh --nj $nj --cmd "$train_cmd" --delta-window 2 \
+				$class/all 32 exp/diag_ubm_${class}_all
+			sid/train_full_ubm.sh --nj $nj --cmd "$train_cmd" \
+				--remove-low-count-gaussians false $class/all \
+				exp/diag_ubm_${class}_all exp/full_ubm_${class}_all
+		done
+	fi
 fi
 
 
