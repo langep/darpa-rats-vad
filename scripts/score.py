0f6534280b4cdd8b64527a457dce58ba45880ca0
langep <patrick.l.lange@gmail.com>
2017-12-17 15:47:20 -0800

    Adding scoring scripts

---
+++ scripts/score.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import sys
+import os
+from decimal import Decimal
+from glob import glob
+
+mapping = {'NS': '0', 'S': '1', 'NT': '2', 'RX': '-1'}
+ignore_rx = {'NS': '0', 'S': '1', 'NT': '2'}
+
+
+def read_targets(path_to_ground_truth_file):
+    with open(path_to_ground_truth_file, 'r') as fh:
+        line = fh.readline()
+        line = line.rstrip()
+        targets = line.split()
+        return targets
+
+
+def compute_stats(matrix, mapping):
+    total = 0
+    correct = 0
+    for key in mapping.keys():
+        for value in mapping.values():
+            total += matrix[mapping[key]][value]
+            if mapping[key] == value:
+                correct += matrix[mapping[key]][value]
+    accuracy = round(Decimal(float(correct/total)), 2)
+    return total, correct, accuracy
+
+
+def print_confusion_matrix(matrix):
+    print("\t" + "\t".join(mapping.keys()))
+
+    lines = []
+    for key in mapping.keys():
+        line = [key]
+        for value in mapping.values():
+            line.append(str(matrix[mapping[key]][value]))
+        lines.append(line)
+
+    for line in lines:
+        print("\t".join(line))
+
+    print()
+    total, correct, accuracy = compute_stats(matrix, mapping)
+    print("Total: {0}, Correct: {1}, Accuracy: {2}".format(total, correct, accuracy))
+    total_rx, correct_rx, accuracy_rx = compute_stats(matrix, ignore_rx)
+    print()
+    print("Ignoring RX")
+    print("Total: {0}, Correct: {1}, Accuracy: {2}".format(total_rx, correct_rx, accuracy_rx))
+
+
+if __name__ == '__main__':
+    ground_truth_dir = sys.argv[1]
+    exp_dir = sys.argv[2]
+    hypotheses_files = glob(os.path.join(exp_dir, 'vad_gmm_*.ark'))
+
+    confusion_matrix = {
+        '0': {'0': 0, '1': 0, '2': 0, '-1': 0},
+        '1': {'0': 0, '1': 0, '2': 0, '-1': 0},
+        '2': {'0': 0, '1': 0, '2': 0, '-1': 0},
+        '-1': {'0': 0, '1': 0, '2': 0, '-1': 0}
+    }
+
+    for file in hypotheses_files:
+        with open(file, 'r') as fh:
+            for line in fh.readlines():
+                parts = line.rstrip().partition(' ')
+                utterance_name = parts[0]
+                ground_truth_name = utterance_name + '.ground_truth'
+                hypotheses = parts[2]
+                hypotheses = hypotheses.replace('[ ', '')
+                hypotheses = hypotheses.replace(' ]', '')
+                hypotheses = hypotheses.split()
+                targets = read_targets(os.path.join(ground_truth_dir, ground_truth_name))
+                for i, h in enumerate(hypotheses):
+                    t = targets[i]
+                    confusion_matrix[h][t] += 1
+
+    print_confusion_matrix(confusion_matrix)
---
+++ scripts/tab_to_ground_truth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import os
+from decimal import Decimal
+
+
+mapping = {'NS': 0, 'S': 1, 'NT': 2, 'RX': -1}
+
+if __name__ == '__main__':
+    tab_filename = sys.argv[1]
+    output_path = sys.argv[2]
+    output_filename = tab_filename.replace('.tab', '.ground_truth')
+    ground_truth = []
+    with open(tab_filename, 'r') as fh:
+        for line in fh.readlines():
+            cols = line.split()
+            target = mapping[cols[4]]
+            start = Decimal(float(cols[2]))
+            end = Decimal(float(cols[3]))
+            # Round to frame-shift precision (10ms)
+            # Multiply by 100 to convert to frame number
+            # inclusive end
+            start_frame = int(round(start, 2) * 100)
+            # Exclusive end
+            end_frame = int(round(end, 2) * 100)
+            segment_length = end_frame - start_frame
+            ground_truth += [str(target)] * segment_length
+    with open(os.path.join(output_path, output_filename), 'w') as fh:
+        fh.write(" ".join(ground_truth))
