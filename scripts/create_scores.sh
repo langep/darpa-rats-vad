149cb073c7b0287225509946ea7251c26a545cf1
langep <patrick.l.lange@gmail.com>
2017-12-18 00:05:52 -0800

    Adding script to create all scores

---
+++ scripts/create_scores.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#/ Usage: bash create_scores.sh
+#/ Description: Produces scores for all run experiments.
+#/ Options:
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+locate_self() {
+	# Locate this script.
+	readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	readonly PARENT_DIR="$SCRIPT_DIR"/..
+}
+
+# Prevent execution when sourcing this script
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+	locate_self
+	cd $PARENT_DIR
+	export LC_ALL=C
+	source ./cmd.sh
+	source ./path.sh
+
+	datadir=$(cat .data_dir)
+
+	channels="A B C D E F G H src"
+	used_classes="NS NT S"
+
+	mkdir scores
+
+	for channel in $channels; do
+		python script/score.py ground_truth/ exp/test_$channel > scores/$channel.scores
+		python script/score.py ground_truth/ exp/test_all_$channel > scores/all_$channel.scores
+	done
+
+	python script/score.py ground_truth/ exp/test_all > scores/all.scores
+fi
