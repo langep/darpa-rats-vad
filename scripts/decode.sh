e4d3c9f24ce964b7e366c8d1b04b4babaa6d3b92
langep <patrick.l.lange@gmail.com>
2017-12-17 19:30:42 -0800

    Adding decoding with joint model for individual channels

--- scripts/decode.sh
+++ scripts/decode.sh
@@ -7,17 +7,37 @@ cd $PARENT_DIR
 source ./cmd.sh
 source ./path.sh
 
-sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/all exp/full_ubm_NS_all exp/full_ubm_S_all exp/full_ubm_NT_all exp/test_all exp/test_all
+stage=$1
 
-channels="A B C D E F H"
-used_classes="NS NT S"
-for channel in $channels; do
-	sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/$channel exp/full_ubm_NS_$channel exp/full_ubm_S_$channel exp/full_ubm_NT_$channel exp/test_$channel exp/test_$channel
-done
+if [ $stage -le 1 ]; then
+	sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/all exp/full_ubm_NS_all exp/full_ubm_S_all exp/full_ubm_NT_all exp/test_all exp/test_all
 
-# Not using NT model as we didn't train it for these channels
-channels="G src"
-used_classes="NS S"
-for channel in $channels; do
-	sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/$channel exp/full_ubm_NS_$channel exp/full_ubm_S_$channel exp/test_$channel exp/test_$channel
-done
+	channels="A B C D E F H"
+	used_classes="NS NT S"
+	for channel in $channels; do
+		sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/$channel exp/full_ubm_NS_$channel exp/full_ubm_S_$channel exp/full_ubm_NT_$channel exp/test_$channel exp/test_$channel
+	done
+
+	# Not using NT model as we didn't train it for these channels
+	channels="G src"
+	used_classes="NS S"
+	for channel in $channels; do
+		sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/$channel exp/full_ubm_NS_$channel exp/full_ubm_S_$channel exp/test_$channel exp/test_$channel
+	done
+fi
+
+if [ $stage -le 2 ]; then
+	# Using joint model on individual channels to evaluate performance per channel
+	channels="A B C D E F H"
+	used_classes="NS NT S"
+	for channel in $channels; do
+		sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/all exp/full_ubm_NS_all exp/full_ubm_S_all exp/full_ubm_NT_all exp/test_all_$channel exp/test_all_$channel
+	done
+
+	# Not using NT model as we didn't train it for these channels
+	channels="G src"
+	used_classes="NS S"
+	for channel in $channels; do
+		sid/compute_vad_decision_gmm.sh --nj 10 --use_energy_vad false test/all exp/full_ubm_NS_all exp/full_ubm_S_all exp/test_all_$channel exp/test_all_$channel
+	done
+fi
